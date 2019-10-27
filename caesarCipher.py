b747f83cf663596610f97d6ae18c1fcf5218dde0
Stephen Clements <mrevilg@gmail.com>
2019-10-27 17:50:13 -0500

    All Symbols

--- caesarCipher.py
+++ caesarCipher.py
@@ -10,3 +10,9 @@ key = 13
 
 # Wheather the program encrypts or decrypts:
 mode = 'encrypt' # Set to eiterh encrypt or decrypt.
+
+# Every possible symbol that can be encrypted:
+SYMBOLS = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz1234567890 !?.'
+
+# Store encrypted/decrypted form.
+translated = ''
