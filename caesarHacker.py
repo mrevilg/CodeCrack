6e0a2d2346b3be7d32cbb66fe70683f3c6c66ef8
Stephen Clements <mrevilg@gmail.com>
2019-10-31 06:32:44 -0500

    Hacker Program

---
+++ caesarHacker.py
@@ -0,0 +1,8 @@
+
+
+message = 'guv6Jv6Jz!J6rp5r7Jzr66ntrM'
+SYMBOLS = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz1234567890 !?.'
+
+# Loop through every possible key:
+for key in range(len(SYMBOLS)):
+    translated = ''
