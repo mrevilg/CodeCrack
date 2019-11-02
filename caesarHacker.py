ea060dcaceefeadbbb170ef9387e9f86b2c55f6e
Stephen Clements <mrevilg@gmail.com>
2019-11-02 09:35:42 -0500

    Final Code

--- caesarHacker.py
+++ caesarHacker.py
@@ -11,3 +11,16 @@ for key in range(len(SYMBOLS)):
         if symbol in SYMBOLS:
             symbolIndex = SYMBOLS.find(symbol)
             translatedIndex = symbolIndex - key
+
+            # Handle the wraparound:
+            if translatedIndex < 0:
+                translatedIndex = translatedIndex + len(SYMBOLS)
+
+            # Append the decrypted symbol:
+            translated = translated + SYMBOLS[translatedIndex]
+
+        else:
+            # Append the symbol without encrypting/decrypting:
+            translated = translated + symbol
+
+    print('Key #%s: %s' % (key, translated))
