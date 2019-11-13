6261b89758f9832bd475fcaec9f39008a07f0023
Stephen Clements <mrevilg@gmail.com>
2019-11-12 19:27:11 -0600

    Transposition Decrypt

--- transpositionDecrypt.py
+++ transpositionDecrypt.py
@@ -3,3 +3,9 @@ import math, pyperclip
 def main():
     myMessage = 'Cenoonommstmme oo snnio. s s c'
     myKey = 8
+
+    plaintext = dectyptMessage(myKey, myMessage)
+
+    print(plaintext + '|')
+
+    pyperclip.copy(plaintext)
