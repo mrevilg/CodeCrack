de85e798c77a3d5e6fda640e06496192a2cf0142
Stephen Clements <mrevilg@gmail.com>
2019-11-18 06:26:37 -0600

    Randomizer Code

--- transpositionTest.py
+++ transpositionTest.py
@@ -3,3 +3,8 @@ import random, sys, transpositionEncrypt, transpositionDecrypt
 
 def main():
     random.seed(42)
+
+    for i in range(20): 
+        message = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ' * random.randint(4,40)
+
+        message = list(message)
