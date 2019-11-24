410e7b9b84723b6b3ee3743343d9cf5b7d9ea3eb
Stephen Clements <mrevilg@gmail.com>
2019-11-24 15:45:15 -0600

    Range Loop

--- transpositionTest.py
+++ transpositionTest.py
@@ -12,3 +12,7 @@ def main():
         message = ''.join(message)
 
         print('Test #%s: "%s..."' % (i + 1, message[:50]))
+
+        for key in range(1, int(len(message)/2)):
+            encrypted = transpositionEncrypt.encryptMessage(key, message)
+            decrypted = transpositionDecrypt.decryptMessage(key, encrypted)
