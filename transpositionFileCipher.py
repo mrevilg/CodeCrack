439431450f4fefff45ce06f65aa43408e1939545
Stephen Clements <mrevilg@gmail.com>
2019-12-02 06:42:50 -0600

    Def Main

--- transpositionFileCipher.py
+++ transpositionFileCipher.py
@@ -4,3 +4,6 @@ import time, os, sys, transpositionEncrypt, transpositionDecrypt
 
 def main():
     inputFilename = 'frankenstein.txt'
+    outputFilename = 'frankenstein.encrypted.txt'
+    myKey = 10
+    myMode = 'encrypt' # Set to encrypt or decrypt
