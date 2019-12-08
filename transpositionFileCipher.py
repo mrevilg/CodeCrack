11a4b5b6cddcf3ffcdc9259e1db51c88a02be54a
Stephen Clements <mrevilg@gmail.com>
2019-12-08 14:16:04 -0600

    Code File Encrypted or Decrypted

--- transpositionFileCipher.py
+++ transpositionFileCipher.py
@@ -3,10 +3,10 @@
 import time, os, sys, transpositionEncrypt, transpositionDecrypt
 
 def main():
-    inputFilename = 'frankenstein.txt'
-    outputFilename = 'frankenstein.encrypted.txt'
+    inputFilename = 'frankenstein.encrypted.txt'
+    outputFilename = 'frankenstein.decrypted.txt'
     myKey = 10
-    myMode = 'encrypt' # Set to encrypt or decrypt
+    myMode = 'decrypt' # Set to encrypt or decrypt
 
     if not os.path.exists(inputFilename):
         print('This file %s does not exist, hold for processing...'
