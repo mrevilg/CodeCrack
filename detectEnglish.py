f04c9603b553f2c8b7bdb612408c8bfd1111c91b
Stephen Clements <mrevilg@gmail.com>
2019-12-11 06:19:13 -0600

    Module

---
+++ detectEnglish.py
@@ -0,0 +1,7 @@
+
+
+UPPERLETTERS = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+LETTERS_AND_SPACE = UPPERLETTERS + UPPERLETTERS.lower() + ' \t\n'
+
+def loadDictionary():
+    dictionaryFile = open('dictionary.txt')
