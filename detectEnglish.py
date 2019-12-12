6309ad06aae16fbbb1c1db23c6c854a1d3857c77
Stephen Clements <mrevilg@gmail.com>
2019-12-12 06:36:40 -0600

    Load TXT file

--- detectEnglish.py
+++ detectEnglish.py
@@ -5,3 +5,10 @@ LETTERS_AND_SPACE = UPPERLETTERS + UPPERLETTERS.lower() + ' \t\n'
 
 def loadDictionary():
     dictionaryFile = open('dictionary.txt')
+    englishWords = {}
+    for word in dictionaryFile.read().split('\n'):
+        englishWords[word] = None
+    dictionaryFile.close()
+    return englishWords
+
+ENGLISH_WORDS = loadDictionary()
