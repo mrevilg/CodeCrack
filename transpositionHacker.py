93dc50b4068db677ec38b4de6b386633cb5665fe
Stephen Clements <mrevilg@gmail.com>
2019-12-16 06:26:32 -0600

    Encrypted Text

--- transpositionHacker.py
+++ transpositionHacker.py
@@ -3,3 +3,11 @@
 import pyperclip, detectEnglish, transpositionDecrypt
 
 def main():
+    myMessage = """AaKoosoeDe5 b5sn ma reno ora'lhlrrceey e  enlh na
+      indeit n uhoretrm au ieu v er Ne2 gmanw,forwnlbsya apor tE.no 
+      euarisfatt  e mealefedhsppmgAnlnoe(c -or)alat r lw o eb  
+      nglom,Ain one dtes ilhetcdba. t tg eturmudg,tfl1e1 v  
+      nitiaicynhrCsaemie-sp ncgHt nie cetrgmnoa yc r,ieaa  
+      toesa- e a0m82e1w shcnth  ekh gaecnpeutaaieetgn iodhso d ro 
+      hAe snrsfcegrt NCsLc b17m8aEheideikfr aBercaeu thllnrshicwsg 
+      etriebruaisss  d iorr."""
