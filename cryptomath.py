9fa8b988e1b772e6a293f880a9c7632985324b53
Stephen Clements <mrevilg@gmail.com>
2019-12-24 05:53:29 -0600

    Modular Inverse def

--- cryptomath.py
+++ cryptomath.py
@@ -5,3 +5,10 @@ def gcd(a, b):
     while a != 0:
         a, b = b % a, a
     return b
+
+def findModInverse(a, m):
+    # Return the modular inverse of a % m, which is
+    # the number x such that a*x % m =1.
+
+    if gcd(a, m) != 1:
+        return None
