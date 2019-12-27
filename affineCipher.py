d2b6b3c6e33dfac9882a8c3c01696dcccc02e98c
Stephen Clements <mrevilg@gmail.com>
2019-12-27 08:24:10 -0600

    Encryption Logic

--- affineCipher.py
+++ affineCipher.py
@@ -4,3 +4,12 @@ SYMBOLS = """ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnop
             qrstuvwxyz1234567890 !?."""
 
 def main():
+    myMessage = """ "A computer would deserve to be called intelligent
+        if it could deceive a human into believing it was human."
+        -Alan Turing"""
+    myKey = 2894
+    myMode = 'encrypt' # Set to either encrypt or decrypt.
+
+    if myMode == 'encrypt':
+        translated = encryptedMessage(myKey, myMessage)
+    elif myMode == 'decrypt'
