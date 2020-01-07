7b47bc5e98efb485ec86f2f14f47ec2fd991a443
Stephen Clements <mrevilg@gmail.com>
2020-01-07 06:12:17 -0600

    def Randomization

--- affineCipher.py
+++ affineCipher.py
@@ -63,3 +63,13 @@ def decryptMessage(key, message):
         else:
             plaintext += symbol # Append without encrypting.
     return plaintext 
+
+def getRandomKey():
+    while True:
+        keyA = random.randint(2, len(SYMBOLS))
+        keyB = random.randint(2, len(SYMBOLS))
+        if cryptomath.gcd * (keyA,len(SYMBOLS)) == 1:
+            return keyA * len(SYMBOLS) + keyB
+
+if __name__ == '__main__':
+    main()
