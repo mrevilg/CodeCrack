fe1a82b8f39f8b25aad552921a1f95efe0b2f233
Stephen Clements <mrevilg@gmail.com>
2020-01-19 07:41:49 -0600

    def main

--- simpleSubCipher.py
+++ simpleSubCipher.py
@@ -3,3 +3,13 @@ import pyperclip, sys, random
 
 
 LETTERS = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+
+def main():
+    myMessage = """If a man is offered a fact which goes against his instincts, 
+        he will scrutinize it closely, and unless the evidence is overwhelming, 
+        he will refuse to believe it. If, on the other hand, he is offered something 
+        which affords a reason for acting in accordance to his instincts, 
+        he will accept it even on the slightest evidence. 
+        The origin of myths is explained in this way. -Bertrand Russell"""
+    myKey = 'LFWOAYUISVKMNXPBDCRJTQEGHZ'
+    myMode = 'encrypt' # Set to encrypt or decrypt
