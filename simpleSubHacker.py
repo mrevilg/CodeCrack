0706f54a84c747c11e4788de5bcf4045b72f9a9c
Stephen Clements <mrevilg@gmail.com>
2020-01-31 05:54:22 -0600

    def Main

--- simpleSubHacker.py
+++ simpleSubHacker.py
@@ -3,3 +3,22 @@
 import os, re, copy, pyperclip, simpleSubCipher, wordPatterns, makeWordPatterns
 
 LETTERS = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+
+def main():
+    message = 'Sy l nlx sr pyyacao l ylwj eiswi upar lulsxrj isr sxrjsxwjr, ia esmm rwctjsxsza sj wmpramh, lxo txmarr jia aqsoaxwa sr pqaceiamnsxu, ia esmm caytra jp famsaqa sj. Sy, px jia pjiac ilxo, ia sr pyyacao rpnajisxu eiswi lyypcor l calrpx ypc lwjsxu sx lwwpcolxwa jp isr sxrjsxwjr, ia esmm lwwabj sj aqax px jia rmsuijarj aqsoaxwa. Jia pcsusx py nhjir sr agbmlsxao sx jisr elh. -Facjclxo Ctrramm'
+
+    # Determine the possible valid ciphertext translations.
+    print('Hacking...')
+    letterMapping = hackSimpleSub(message)
+
+    # Display the results to the user.
+    print('Mapping:')
+    pprint.pprint(letterMapping)
+    print()
+    print('Original ciphertext:')
+    print(message)
+    print()
+    print('Copying hacked message to clipboard:')
+    hackedMessage = decryptWithCipherletterMapping(message, letterMapping)
+    pyperclip.copy(hackedMessage)
+    print(hackedMessage)
