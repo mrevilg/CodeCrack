1df6da1379b7a5e3984726cbafc0798f46abc94f
Stephen Clements <mrevilg@gmail.com>
2020-02-10 05:49:19 -0600

    main variables

---
+++ vigenereCipher.py
@@ -0,0 +1,11 @@
+
+
+import pyperclip
+
+LETTERS = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+
+def main():
+    myMessage = """Alan Mathison Turing was a British mathematician, 
+        logician, cryptanalyst, and computer scientist."""
+    myKey = 'ASIMOV'
+    myMode = 'encrypt' # Set to encrypt or decrypt
