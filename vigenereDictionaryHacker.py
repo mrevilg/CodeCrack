7c84ac3e96e66b5968a60b272e1d574005167fd5
Stephen Clements <mrevilg@gmail.com>
2020-02-24 06:43:40 -0600

    def main complete

--- vigenereDictionaryHacker.py
+++ vigenereDictionaryHacker.py
@@ -4,3 +4,13 @@ import detectEnglish, vigenereCipher, pyperclip
 def main():
     ciphertext = """Tzx isnz eccjxkg nfq lol mys bbqq I lxcz."""
     hackedMessage = hackVigenere(ciphertext)
+
+    if hackedMessage != None:
+        print('Copying Hacked message to clipboard:')
+        print(hackedMessage)
+        pyperclip.copy(hackedMessage)
+    else:
+        print('Failed to hack encryption.')
+
+def hackVigenereDictionary(ciphertext):
+    fo = open('dictionary.txt')
