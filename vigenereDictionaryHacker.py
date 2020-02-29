ac78ce68045eae1b3d8720bd055d4f87e8d90831
Stephen Clements <mrevilg@gmail.com>
2020-02-29 15:42:04 -0600

    Bug Fixing

--- vigenereDictionaryHacker.py
+++ vigenereDictionaryHacker.py
@@ -3,7 +3,7 @@ import detectEnglish, vigenereCipher, pyperclip
 
 def main():
     ciphertext = """Tzx isnz eccjxkg nfq lol mys bbqq I lxcz."""
-    hackedMessage = hackVigenere(ciphertext)
+    hackedMessage = hackVigenereDictionary(ciphertext)
 
     if hackedMessage != None:
         print('Copying Hacked message to clipboard:')
@@ -15,9 +15,9 @@ def main():
 def hackVigenereDictionary(ciphertext):
     fo = open('dictionary.txt')
     words = fo.readlines()
-    fo.close
+    fo.close()
 
-    for word in lines:
+    for word in words:
         word = word.strip()
         decryptedText = vigenereCipher.decryptMessage(word, ciphertext)
         if detectEnglish.isEnglish(decryptedText, wordPercentage=40):
@@ -26,7 +26,6 @@ def hackVigenereDictionary(ciphertext):
             print('Key ' + str(word + ': ' + decryptedText[:100]))
             print()
             print('Enter D for done, or press Enter to continue breaking:')
-
             response = input('> ')
 
             if response.upper().startswith('D'):
