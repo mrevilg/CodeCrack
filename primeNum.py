f521ef1b98b1eb557b4699737f10641a673473c4
Stephen Clements <mrevilg@gmail.com>
2020-03-04 06:08:51 -0600

    Number divisable? #2

--- primeNum.py
+++ primeNum.py
@@ -5,3 +5,8 @@ import math, random
 def isPrimeTrialDiv(num):
     if num < 2:
         return False
+
+    for i in range (2, int(math.sqrt(num)) + 1):
+        if num % i == 0:
+            return False
+    return True
