f39306851998d1e691d87610873377a8ceb7f47f
Stephen Clements <mrevilg@gmail.com>
2020-03-08 13:43:51 -0500

    Sieve #4

--- primeNum.py
+++ primeNum.py
@@ -19,3 +19,8 @@ def primeSieve(sieveSize):
 
     for i in range(2, int(math.sqrt(sieveSize)) + 1):
         pointer = i * 2
+        while pointer < sieveSize:
+            sieve[pointer] = False
+            pointer += i
+        
+        primes = []
