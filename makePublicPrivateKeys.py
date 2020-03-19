c93e775b46fcda91d014ea7a143e482f5348a23e
Stephen Clements <mrevilg@gmail.com>
2020-03-19 06:45:15 -0500

    def generateKey

--- makePublicPrivateKeys.py
+++ makePublicPrivateKeys.py
@@ -6,3 +6,9 @@ def main():
     print('Making key files...')
     makeKeyFiles('al_sweigart', 1024)
     print('Key files made.')
+
+def generateKey(keysize):
+    p = 0
+    q = 0
+
+    print('Generating p prime...')
