e4da04217df13852fc9236a6e442158f11573058
Stephen Clements <mrevilg@gmail.com>
2020-03-22 08:02:11 -0500

    def generateKey #4

--- makePublicPrivateKeys.py
+++ makePublicPrivateKeys.py
@@ -19,5 +19,7 @@ def generateKey(keysize):
 
     print('Generating e that is relatively prime to (p-1)*(q-1)...')
     while True:
+        e = random.randrange(2 ** (keysize - 1), 2 ** (keysize))
+        if cryptomath.gcd(e, (p - 1) * (q - 1))
         
         
