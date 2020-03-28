0ce49d70f095bbd95e540e757e552c918ea3f9fd
Stephen Clements <mrevilg@gmail.com>
2020-03-28 08:26:55 -0500

    Bug Fix 3/25/20

--- cryptomath.py
+++ cryptomath.py
@@ -17,6 +17,5 @@ def findModInverse(a, m):
     v1, v2, v3 = 0, 1, m
     while v3 != 0:
         q = u3 // v3 # Note: // is the int division operator.
-        v1, v2, v3, u1, u2, u3, = (u1 - q * v1), (u2 - q * v2), 
-        (u3 - q * v3), v1, v2, v3
+        v1, v2, v3, u1, u2, u3, = (u1 - q * v1), (u2 - q * v2), (u3 - q * v3), v1, v2, v3
     return u1 % m
